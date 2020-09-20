03f4f07ec51bdfe6c0475408111d03c6bba26adf
gokeen10 <70183453+gokeen10@users.noreply.github.com>
2020-09-20 17:25:40 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,18 @@
+class paper {
+    constructor(x,y,height,angle){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+    }
+
+    display(){
+
+       ellipseMode(RADIUS) 
+       ellipse(20, 20, 30, 30) 
+    
+    }
+}
+
